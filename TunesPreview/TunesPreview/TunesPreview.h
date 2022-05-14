ff0ff8fc371ec7e2ebbd4c518343d1067c5fccfc
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-05-14 16:47:51 +0300

    Add the starter project

---
+++ TunesPreview/TunesPreview/TunesPreview.h
@@ -0,0 +1,18 @@
+//
+//  TunesPreview.h
+//  TunesPreview
+//
+//  Created by Vladimir Mironiuk on 14.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TunesPreview.
+FOUNDATION_EXPORT double TunesPreviewVersionNumber;
+
+//! Project version string for TunesPreview.
+FOUNDATION_EXPORT const unsigned char TunesPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TunesPreview/PublicHeader.h>
+
+
